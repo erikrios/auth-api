f45cbcaff1c67c74321205721d341a631d7b2b63
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2021-11-13 10:07:29 +0700

    Pass say hello world test case

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -31,19 +31,19 @@ describe('HTTP server', () => {
 
   describe('when GET /', () => {
     it('should return 200 and hello world', async () => {
-     // Arrange
-     const server = await createServer({});
+      // Arrange
+      const server = await createServer({});
 
-     // Action
-     const response = await server.inject({
-       method: 'GET',
-       url: '/',
-     });
+      // Action
+      const response = await server.inject({
+        method: 'GET',
+        url: '/',
+      });
 
-     // Assert
+      // Assert
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(200);
-      expect(responseJson.value).toEqual('Helo world!');
+      expect(responseJson.value).toEqual('Hello world!');
     });
   });
 
